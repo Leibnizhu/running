99002bf2b92690e22c18f3ecd6eaf96149ad49fc
Leibniz.Hu <leibnizhu@gmail.com>
2022-04-16 09:48:14 +0800

    chore(): 同步数据

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  pathPrefix: '/', // Change to `/running_page` when running on github pages
+  pathPrefix: '/running', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Workouts Map',
-    siteUrl: 'https://ben29.xyz',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    siteUrl: 'https://leibnizhu.gitlab.io/running',
+    logo: 'https://avatars.githubusercontent.com/u/13050963?s=96&v=4',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://ben29.xyz',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/ben-29/running_page/blob/master/README-CN.md',
-      },
+        url: 'https://leibnizhu.gitlab.io/',
+      }
     ],
   },
   plugins: [
